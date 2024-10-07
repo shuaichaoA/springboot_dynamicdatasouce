20379120fdea2a963e4ba14ef9b82677a1238c4c
shuaichaoA <271085131@qq.com>
2024-10-07 23:12:31 +0800

    init

---
+++ 03-dynamic_datasource_framework/src/main/java/com/tuling/dynamic/datasource/controller/DataSourceController.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright © ${project.inceptionYear} organization baomidou
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tuling.dynamic.datasource.controller;
+
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
+import com.baomidou.dynamic.datasource.creator.DefaultDataSourceCreator;
+import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DataSourceProperty;
+import com.tuling.dynamic.datasource.dto.DataSourceDTO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.sql.DataSource;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/datasources")
+public class DataSourceController {
+
+    private final DataSource dataSource;
+    private final DefaultDataSourceCreator dataSourceCreator;
+
+    public DataSourceController(DataSource dataSource, DefaultDataSourceCreator dataSourceCreator) {
+        this.dataSource = dataSource;
+        this.dataSourceCreator = dataSourceCreator;
+    }
+
+    /**
+     * 获取当前所有数据源
+     */
+    @GetMapping
+    public Set<String> now() {
+        DynamicRoutingDataSource ds = (DynamicRoutingDataSource) dataSource;
+        return ds.getDataSources().keySet();
+    }
+
+    /**
+     * 添加数据源
+     */
+    @PostMapping
+    public Set<String> add(@Validated @RequestBody DataSourceDTO dto) {
+        DataSourceProperty dataSourceProperty = new DataSourceProperty();
+        BeanUtils.copyProperties(dto, dataSourceProperty);
+        DynamicRoutingDataSource ds = (DynamicRoutingDataSource) dataSource;
+        DataSource dataSource = dataSourceCreator.createDataSource(dataSourceProperty);
+        ds.addDataSource(dto.getPoolName(), dataSource);
+        return ds.getDataSources().keySet();
+    }
+
+    /**
+     * 删除数据源
+     */
+    @DeleteMapping
+    public String remove(@RequestParam String name) {
+        DynamicRoutingDataSource ds = (DynamicRoutingDataSource) dataSource;
+        ds.removeDataSource(name);
+        return "删除成功";
+    }
+}
